a3e7e5d99dc04c7b8a16de2473e8ab239d34520d
Dylan Donnell <dylan.donnell@student.griffith.ie>
2024-11-27 20:19:48 +0000

    style(misc): better formatting

--- src/commands/misc.rs
+++ src/commands/misc.rs
@@ -4,52 +4,53 @@ use crate::{
 };
 use sysinfo::System;
 
+/// Show system information
 #[poise::command(prefix_command, slash_command)]
 pub async fn sysinfo(ctx: Context<'_>) -> Result<(), Error> {
     // System info
     let system = System::new_all();
 
     let name = format!(
-        "Name: `{}`",
+        "**Name:**\t`{}`",
         System::name().unwrap_or("N/A".to_string())
     );
     let kernel = format!(
-        "Kernel: `{}`",
+        "**Kernel:**\t`{}`",
         System::kernel_version().unwrap_or("N/A".to_string())
     );
     let version = format!(
-        "Version: `{}`",
+        "**Version:**\t`{}`",
         System::os_version().unwrap_or("N/A".to_string())
     );
     let hostname = format!(
-        "Hostname: `{}`",
+        "**Hostname:**\t`{}`",
         System::host_name().unwrap_or("N/A".to_string())
     );
     let uptime = format!(
-        "Uptime: `{}`",
+        "**Uptime:**\t`{}`",
         pretty_seconds(System::uptime())
     );
     let cpus = format!(
-        "CPUs: `{}`",
+        "**CPUs:**\t`{}`",
         system.cpus().len()
     );
     let memory = format!(
-        "Memory: `{}` / `{}`",
+        "**Memory:**\t`{}`\t**/**\t`{}`",
         pretty_bytes(system.used_memory()),
         pretty_bytes(system.total_memory())
     );
     let swap = format!(
-        "Swap: `{}` / `{}`",
+        "**Swap:**\t`{}`\t**/**\t`{}`",
         pretty_bytes(system.used_swap()),
         pretty_bytes(system.total_swap())
     );
 
     // Package info
-    let bot = format!("markov-chains-bot: `{}`", env!("CARGO_PKG_VERSION"));
-    let markov = format!("markov-chains: `{}`", MARKOV_CHAINS_VERSION);
+    let bot = format!("**markov-chains-bot:** `{}`", env!("CARGO_PKG_VERSION"));
+    let markov = format!("**markov-chains:** `{}`", MARKOV_CHAINS_VERSION);
 
     ctx.say(format!(
-        "## System\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n\n## Package\n{}\n{}",
+        "## System\n- {}\n- {}\n- {}\n- {}\n- {}\n- {}\n- {}\n- {}\n## Package\n- {}\n- {}",
         name, kernel, version, hostname, uptime, cpus, memory, swap, bot, markov
     )).await?;
 
